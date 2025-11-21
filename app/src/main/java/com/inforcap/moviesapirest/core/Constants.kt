c783302f20a4f31ab0d7bc37ba8f8e765ba4f71f
Christian Sarmiento Zampillo <christian.sarmiento@gmail.com>
2025-11-21 19:53:26 -0300

    Update API_KEY placeholder in Constants.kt

--- app/src/main/java/com/inforcap/moviesapirest/core/Constants.kt
+++ app/src/main/java/com/inforcap/moviesapirest/core/Constants.kt
@@ -2,7 +2,7 @@ package com.inforcap.moviesapirest.core
 
 object Constants {
 
-    const val API_KEY = "fa625d58ec8ff215eb2eb7cdb472d218"
+    const val API_KEY = "DEBEN INGRESAR LA API_KEY QUE LES ENVIE POR CORREO"
     const val API_URL = "https://api.themoviedb.org/3/movie/"
     const val API_URL_IMAGE = "https://image.tmdb.org/t/p/w500/"
     const val IMAGE_WIDTH = 400
